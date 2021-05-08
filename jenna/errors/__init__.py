1ba7d092816168ac4fccca442e0f6a08501ab391
GoZaddy <faruqyusuff437@gmail.com>
2021-05-08 00:01:51 -0500

    some minor updates

--- jenna/errors/__init__.py
+++ jenna/errors/__init__.py
@@ -1,6 +1,6 @@
 class CodegenError(Exception):
     """
-    This exception is raised when an error occurs or an invalid value is encountered in the Codegen
+    This exception is raised when an error occurs or an invalid value is encountered in Jenna
     """
 
     def __init__(self, message):
--- main.py
+++ main.py
@@ -12,5 +12,16 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+from jenna import Jenna, Variable, String
+
+jen = Jenna()
+
+jen.write_variable(
+    Variable(
+        name='variable_name',
+        value=String('variable_value')
+    )
+)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
